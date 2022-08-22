44ca65a5b3beb5e89a1536f7347f9c23339d5175
Giorgi Bakradze <Bakradzegeorge17@gmail.com>
2022-08-23 00:50:48 +0400

    Adds lambda expression demo tests

--- src/main/java/com/example/LambdaExpressions.java
+++ src/main/java/com/example/LambdaExpressions.java
@@ -3,19 +3,16 @@ package com.example;
 import java.util.function.Function;
 
 public class LambdaExpressions {
-    public static void main(String[] args) {
-        Function<Integer, Integer> myFunc = (Integer x) -> x * 2;
+    public Function<Integer, Integer> myFunc = (Integer x) -> x * 2;
 
-     //   Function<String, String> anotherFunc = (String name) -> "Hello " + name;
+    //   Function<String, String> anotherFunc = (String name) -> "Hello " + name;
 
-        Function<String, String> anotherFunc = name -> "Hello " + name;
+    public Function<String, String> anotherFunc = name -> "Hello " + name;
 
-        Function<String, Integer> wordLengthCalculator =
-                (String word) -> {
-                    Integer numOfChars = word.length();
-                    String result = "String has " + numOfChars + " characters.";
-                    return Integer.valueOf(result);
-                };
+    public Function<String, Integer> wordLengthCalculator =
+            (String word) -> {
+                Integer numOfChars = word.length();
+                return numOfChars;
+            };
 
-    }
 }
