a02a110674f93fc1e384d9ec4f18fd51e2189e5b
Giorgi Bakradze <Bakradzegeorge17@gmail.com>
2022-08-24 02:10:14 +0400

    Refactor for function interface demo

--- src/main/java/com/example/FunctionInterface.java
+++ src/main/java/com/example/FunctionInterface.java
@@ -4,15 +4,8 @@ package com.example;
 import java.util.function.Function;
 
 public class FunctionInterface {
-    static class Introduction {
-        public static String sayHi(String name) {
-            return "Hello " + name;
-        }
-    }
-
     public static void main(String[] args) {
-        Function<String, String> greeting = Introduction::sayHi;
-        System.out.println(greeting.apply("James"));
-        System.out.println(greeting.apply("Scott"));
+        Function<Integer, Integer> squareFunction = (Integer x) -> x * x;
+        squareFunction.apply(2);
     }
 }
